348e794a8ec6977fc28c42d8fbdaba56374bb5fc
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-09-24 04:10:28 +0700

    still trying

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const routes = require("./routes");
 const session = require("express-session");
 
@@ -22,6 +22,6 @@ app.use(
 
 app.use("/", routes);
 
-app.listen(port, () => {
-  console.log(`listening to localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`listening to localhost:${PORT}`);
 });
